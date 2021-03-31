6c0bdf09c0bb992c1a217ac12ca53e92de0d2aa6
AlexandrOkolotovich <60757062+AlexandrOkolotovich@users.noreply.github.com>
2021-03-31 09:10:16 +0300

    Add files via upload

---
+++ constant/RequestParameters.java
@@ -0,0 +1,9 @@
+package by.jwd.restaurant.constant;
+
+public class RequestParameters {
+
+    public static final String LOGIN_EMAIL = "email";
+    public static final String LOGIN_PASSWORD = "password";
+
+    private RequestParameters(){};
+}
---
+++ constant/SessionAttributes.java
@@ -0,0 +1,9 @@
+package by.jwd.restaurant.constant;
+
+public class SessionAttributes {
+    public static final String LOCALE = "locale";
+
+
+    private SessionAttributes() {
+    }
+}
